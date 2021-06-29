fcb470be83c6a8bf72be65a114bde9259d210ee2
LucasFB27 <81167310+LucasFB27@users.noreply.github.com>
2021-06-29 13:07:14 -0300

    Create links_playstore.java

---
+++ links_playstore.java
@@ -0,0 +1,45 @@
+package com.javatpoint.bean;
+
+import java.util.*;
+
+public class links_playstore {
+	private int ID_link, ID_estudemais;
+	private String imagem, nome, link;
+	
+	public int getID_link() {
+		return ID_link;
+	}
+	
+	public void setID_link(int ID_link) {
+		this.ID_link = ID_link;
+	}
+	
+	public int getID_estudemais() {
+		return ID_estudemais;
+	}
+	
+	public void setID_estudemais(int ID_estudemais) {
+		this.ID_estudemais = ID_estudemais;
+	}
+	
+	public String getnome() {
+		return nome;
+	}
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getimagem() {
+		return imagem;
+	}
+	public void setimagem(String imagem) {
+		this.imagem = imagem;
+	}
+	
+	public String getlink() {
+		return link;
+	}
+	public void setlink(String link) {
+		this.link = link;
+	}
+}
